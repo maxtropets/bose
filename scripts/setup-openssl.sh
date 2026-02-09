b8ebf490ae3cd226d63a11b9b931c43284fa04ed
Max Tropets <maxtropets@gmail.com>
2026-02-09 13:35:03 +0000

    Try tests

--- scripts/setup-openssl.sh
+++ scripts/setup-openssl.sh
@@ -2,11 +2,12 @@
 set -ex
 
 curl -LO https://github.com/openssl/openssl/releases/download/openssl-3.5.5/openssl-3.5.5.tar.gz
-tar -xvf openssl-3.5.5.tar.gz 
+tar -xvf openssl-3.5.5.tar.gz
 cd openssl-3.5.5
-./Configure --prefix=/opt/openssl
+./Configure
 make -j$(nproc)
-make install
 
-export OPENSSL_DIR=/opt/openssl
-export LD_PRELOAD=/opt/openssl/lib64/libcrypto.so:/opt/openssl/lib64/libssl.so
+# Run as source ./scripts/setup-openssl.sh to have this set.
+export OPENSSL_INCLUDE_DIR="$PWD/include"
+export OPENSSL_LIB_DIR="$PWD"
+export LD_PRELOAD="$PWD/libcrypto.so:$PWD/libssl.so"
